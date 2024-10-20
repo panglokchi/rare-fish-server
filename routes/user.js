0c6a23d79a8dc59018970a73f3abbf5c7b3baaf2
panglokchi <lokchi.pang@gmail.com>
2024-10-20 02:38:26 +0100

    enabled verification email sending

--- routes/user.js
+++ routes/user.js
@@ -65,7 +65,7 @@ module.exports = async function(app){
 
             await newVerificationToken.save()
 
-            //utils.sendVerificationEmail(req.body.email, token)
+            utils.sendVerificationEmail(req.body.email, token)
 
             res.status(201).json({ message: 'User registered successfully' });
         } catch (error) {
